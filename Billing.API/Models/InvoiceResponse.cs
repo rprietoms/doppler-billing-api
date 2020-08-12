650bbd35dde5e3d9f9a872d2f54007cabd5c3ed7
rockambole86 <enw1986@hotmail.com>
2020-08-12 12:28:14 -0300

    CS8618 fix

--- Billing.API/Models/InvoiceResponse.cs
+++ Billing.API/Models/InvoiceResponse.cs
@@ -4,11 +4,11 @@ namespace Billing.API.Models
 {
     public class InvoiceResponse
     {
-        public string Product { get; set; }
-        public string AccountId { get; set; }
+        public string Product { get; set; } = string.Empty;
+        public string AccountId { get; set; } = string.Empty;
         public DateTime Date { get; set; }
-        public string Currency { get; set; }
+        public string Currency { get; set; } = string.Empty;
         public double Amount { get; set; }
-        public string Link { get; set; }
+        public string Link { get; set; } = string.Empty;
     }
 }
