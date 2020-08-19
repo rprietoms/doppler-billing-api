588205b2dd8faca1b009b0f6f6fc95b7dac9509a
rockambole86 <enw1986@hotmail.com>
2020-08-19 09:37:14 -0300

    Added logic to retrieve data from Hana connection

--- Billing.API/Services/Invoice/InvoiceProviderOptions.cs
+++ Billing.API/Services/Invoice/InvoiceProviderOptions.cs
@@ -6,5 +6,7 @@ namespace Billing.API.Services.Invoice
         public string Host { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public string Schema { get; set; } = string.Empty;
+        public string DbConnectionString { get; set; } = string.Empty;
     }
 }
