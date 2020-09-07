ab88ad8a9defc7ecdfd200f27c396e5f2fd2219e
rockambole86 <enw1986@hotmail.com>
2020-09-07 13:59:29 -0300

    - Added endpoint to download PDF - Added dummy PDF instead of TXT file with incorrect MIME type - Added ID in the invoices list

--- Billing.API/Services/Invoice/InvoiceService.cs
+++ Billing.API/Services/Invoice/InvoiceService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Billing.API.Models;
@@ -23,47 +24,7 @@ namespace Billing.API.Services.Invoice
 
         public async Task<IEnumerable<InvoiceListItem>> GetInvoices(string clientPrefix, int clientId)
         {
-            var schema = _options.Value.Schema;
-
-            await using var conn = new HanaConnection(_options.Value.DbConnectionString);
-
-            await conn.OpenAsync();
-
-            var query = string.Empty;
-
-            query += $" SELECT DISTINCT";
-            query += $"     T0.\"CardCode\" ,";
-            query += $"     T0.\"CardName\" ,";
-            query += $"     T0.\"SendDate\" ,";
-            query += $"     T0.\"SendTime\" ,";
-            query += $"     cast(T0.\"DocEntry\" AS NVARCHAR) AS \"DocEntry\" ,";
-            query += $"     T2.\"DocTotal\" ,";
-            query += $"     T2.\"PaidToDate\" ,";
-            query += $"     cast(T2.\"DocCur\" AS NVARCHAR)   AS \"DocCur\" ,";
-            query += $"     cast(T1.\"trgtPath\" AS NVARCHAR) AS \"trgtPath\" ,";
-            query += $"     cast(T1.\"FileName\" AS NVARCHAR) AS \"FileName\" ,";
-            query += $"     cast(T1.\"FileExt\" AS NVARCHAR)  AS \"FileExt\" ";
-            query += $" FROM";
-            query += $"     {schema}.oeml T0 ";
-            query += $"     INNER JOIN {schema}.ATC1 T1 ON T0.\"AtcEntry\" = T1.\"AbsEntry\" ";
-            query += $"     INNER JOIN {schema}.OINV T2 ON T0.\"DocEntry\" = T2.\"DocNum\" ";
-            query += $"     INNER JOIN ( SELECT";
-            query += $"         T0.\"DocEntry\" ,";
-            query += $"         min(T0.\"SendTime\") AS \"SendTime\" ";
-            query += $"         FROM {schema}.oeml T0 ";
-            query += $"         WHERE T0.\"ObjType\" = '13' ";
-            query += $"         GROUP BY T0.\"DocEntry\" ) x ON x.\"DocEntry\" = T0.\"DocEntry\" AND x.\"SendTime\" = T0.\"SendTime\"";
-            query += $" WHERE";
-            query += $"     T0.\"ObjType\" = '13'";
-            query += $" AND (T0.\"CardCode\" = '{clientPrefix}{clientId:0000000000000}' OR T0.\"CardCode\" LIKE '{clientPrefix}{clientId:00000000000}.%')";
-
-            var da = new HanaDataAdapter(query, conn);
-
-            var dt = new DataTable("Invoices");
-
-            da.Fill(dt);
-
-            await conn.CloseAsync();
+            var dt = await GetInvoiceRecords(clientPrefix, clientId);
 
             return dt.AsEnumerable().Select(dr => new InvoiceListItem(
                 clientPrefix,
@@ -71,7 +32,21 @@ namespace Billing.API.Services.Invoice
                 dr.Field<DateTime>("SendDate").ToDateTimeOffSet(),
                 dr.Field<string>("DocCur"),
                 dr.Field<decimal>("DocTotal").ToDouble(),
-                $"{dr.Field<string>("trgtPath")}\\{dr.Field<string>("FileName")}.{dr.Field<string>("FileExt")}")).ToList();
+                $"invoice_{dr.Field<DateTime>("SendDate"):yyyy-MM-dd}_{dr.Field<int>("AbsEntry")}.{dr.Field<string>("FileExt")}")).ToList();
+        }
+
+        public async Task<byte[]?> GetInvoiceFile(string clientPrefix, int clientId, int fileId)
+        {
+            var dt = await GetInvoiceRecords(clientPrefix, clientId, fileId);
+
+            var dr = dt.AsEnumerable().FirstOrDefault();
+
+            if (dr == null)
+                return null;
+
+            var link = $"{dr.Field<string>("trgtPath")}\\{dr.Field<string>("FileName")}.{dr.Field<string>("FileExt")}";
+
+            return await File.ReadAllBytesAsync(link);
         }
 
         public async Task<string> TestSapConnection()
@@ -102,6 +77,59 @@ namespace Billing.API.Services.Invoice
             }
 
             return "Successfull";
+        }
+
+        private async Task<DataTable> GetInvoiceRecords(string? clientPrefix, int? clientId, int? fileId = null)
+        {
+            var schema = _options.Value.Schema;
+
+            await using var conn = new HanaConnection(_options.Value.DbConnectionString);
+
+            await conn.OpenAsync();
+
+            var query = string.Empty;
+
+            query += $" SELECT DISTINCT";
+            query += $"     T0.\"AbsEntry\" ,";
+            query += $"     T0.\"CardCode\" ,";
+            query += $"     T0.\"CardName\" ,";
+            query += $"     T0.\"SendDate\" ,";
+            query += $"     T0.\"SendTime\" ,";
+            query += $"     cast(T0.\"DocEntry\" AS NVARCHAR) AS \"DocEntry\" ,";
+            query += $"     T2.\"DocTotal\" ,";
+            query += $"     T2.\"PaidToDate\" ,";
+            query += $"     cast(T2.\"DocCur\" AS NVARCHAR)   AS \"DocCur\" ,";
+            query += $"     cast(T1.\"trgtPath\" AS NVARCHAR) AS \"trgtPath\" ,";
+            query += $"     cast(T1.\"FileName\" AS NVARCHAR) AS \"FileName\" ,";
+            query += $"     cast(T1.\"FileExt\" AS NVARCHAR)  AS \"FileExt\" ";
+            query += $" FROM";
+            query += $"     {schema}.oeml T0 ";
+            query += $"     INNER JOIN {schema}.ATC1 T1 ON T0.\"AtcEntry\" = T1.\"AbsEntry\" ";
+            query += $"     INNER JOIN {schema}.OINV T2 ON T0.\"DocEntry\" = T2.\"DocNum\" ";
+            query += $"     INNER JOIN ( SELECT";
+            query += $"         T0.\"DocEntry\" ,";
+            query += $"         min(T0.\"SendTime\") AS \"SendTime\" ";
+            query += $"         FROM {schema}.oeml T0 ";
+            query += $"         WHERE T0.\"ObjType\" = '13' ";
+            query += $"         GROUP BY T0.\"DocEntry\" ) x ON x.\"DocEntry\" = T0.\"DocEntry\" AND x.\"SendTime\" = T0.\"SendTime\"";
+            query += $" WHERE";
+            query += $"     T0.\"ObjType\" = '13'";
+
+            if (clientPrefix.IsNotNullOrEmpty() && clientId.HasValue)
+                query += $" AND (T0.\"CardCode\" = '{clientPrefix}{clientId:0000000000000}' OR T0.\"CardCode\" LIKE '{clientPrefix}{clientId:00000000000}.%')";
+
+            if (fileId.HasValue)
+                query += $" AND (T0.\"AbsEntry\" = '{fileId}')";
+
+            var da = new HanaDataAdapter(query, conn);
+
+            var dt = new DataTable("Invoices");
+
+            da.Fill(dt);
+
+            await conn.CloseAsync();
+
+            return dt;
         }
     }
 }
